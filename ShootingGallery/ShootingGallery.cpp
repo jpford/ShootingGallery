fe2f2078ed23a61f83de2f6fdaaa03def246e893
JP Ford <jpford4@gmail.com>
2013-02-02 16:20:08 -0500

    Initial commit
    Initial commit of base Direct3D source code. Trying to limit the types
    of versioned files to ONLY game assets and source.

---
+++ ShootingGallery/ShootingGallery.cpp
@@ -0,0 +1,149 @@
+﻿#include "pch.h"
+#include "ShootingGallery.h"
+#include "BasicTimer.h"
+
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI::Core;
+using namespace Windows::System;
+using namespace Windows::Foundation;
+using namespace Windows::Graphics::Display;
+using namespace concurrency;
+
+ShootingGallery::ShootingGallery() :
+	m_windowClosed(false),
+	m_windowVisible(true)
+{
+}
+
+void ShootingGallery::Initialize(CoreApplicationView^ applicationView)
+{
+	applicationView->Activated +=
+        ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &ShootingGallery::OnActivated);
+
+	CoreApplication::Suspending +=
+        ref new EventHandler<SuspendingEventArgs^>(this, &ShootingGallery::OnSuspending);
+
+	CoreApplication::Resuming +=
+        ref new EventHandler<Platform::Object^>(this, &ShootingGallery::OnResuming);
+
+	m_renderer = ref new CubeRenderer();
+}
+
+void ShootingGallery::SetWindow(CoreWindow^ window)
+{
+	window->SizeChanged += 
+        ref new TypedEventHandler<CoreWindow^, WindowSizeChangedEventArgs^>(this, &ShootingGallery::OnWindowSizeChanged);
+
+	window->VisibilityChanged +=
+		ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &ShootingGallery::OnVisibilityChanged);
+
+	window->Closed += 
+        ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &ShootingGallery::OnWindowClosed);
+
+	window->PointerCursor = ref new CoreCursor(CoreCursorType::Arrow, 0);
+
+	window->PointerPressed +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &ShootingGallery::OnPointerPressed);
+
+	window->PointerMoved +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &ShootingGallery::OnPointerMoved);
+
+	m_renderer->Initialize(CoreWindow::GetForCurrentThread());
+}
+
+void ShootingGallery::Load(Platform::String^ entryPoint)
+{
+}
+
+void ShootingGallery::Run()
+{
+	BasicTimer^ timer = ref new BasicTimer();
+
+	while (!m_windowClosed)
+	{
+		if (m_windowVisible)
+		{
+			timer->Update();
+			CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
+			m_renderer->Update(timer->Total, timer->Delta);
+			m_renderer->Render();
+			m_renderer->Present(); // This call is synchronized to the display frame rate.
+		}
+		else
+		{
+			CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessOneAndAllPending);
+		}
+	}
+}
+
+void ShootingGallery::Uninitialize()
+{
+}
+
+void ShootingGallery::OnWindowSizeChanged(CoreWindow^ sender, WindowSizeChangedEventArgs^ args)
+{
+	m_renderer->UpdateForWindowSizeChange();
+}
+
+void ShootingGallery::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+{
+	m_windowVisible = args->Visible;
+}
+
+void ShootingGallery::OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
+{
+	m_windowClosed = true;
+}
+
+void ShootingGallery::OnPointerPressed(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	// Insert your code here.
+}
+
+void ShootingGallery::OnPointerMoved(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	// Insert your code here.
+}
+
+void ShootingGallery::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args)
+{
+	CoreWindow::GetForCurrentThread()->Activate();
+}
+
+void ShootingGallery::OnSuspending(Platform::Object^ sender, SuspendingEventArgs^ args)
+{
+	// Save app state asynchronously after requesting a deferral. Holding a deferral
+	// indicates that the application is busy performing suspending operations. Be
+	// aware that a deferral may not be held indefinitely. After about five seconds,
+	// the app will be forced to exit.
+	SuspendingDeferral^ deferral = args->SuspendingOperation->GetDeferral();
+
+	create_task([this, deferral]()
+	{
+		// Insert your code here.
+
+		deferral->Complete();
+	});
+}
+ 
+void ShootingGallery::OnResuming(Platform::Object^ sender, Platform::Object^ args)
+{
+	// Restore any data or state that was unloaded on suspend. By default, data
+	// and state are persisted when resuming from suspend. Note that this event
+	// does not occur if the app was previously terminated.
+}
+
+IFrameworkView^ Direct3DApplicationSource::CreateView()
+{
+    return ref new ShootingGallery();
+}
+
+[Platform::MTAThread]
+int main(Platform::Array<Platform::String^>^)
+{
+	auto direct3DApplicationSource = ref new Direct3DApplicationSource();
+	CoreApplication::Run(direct3DApplicationSource);
+	return 0;
+}
